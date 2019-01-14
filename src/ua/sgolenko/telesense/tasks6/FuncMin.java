47b36844cfbe1c73ad3ae2d527a2d385103c9d49
Sergungo <serjgolenko@gmail.com>
2019-01-14 14:28:34 +0200

    code modified

--- src/ua/sgolenko/telesense/tasks6/FuncMin.java
+++ src/ua/sgolenko/telesense/tasks6/FuncMin.java
@@ -4,6 +4,7 @@ public class FuncMin {
 
     static double minimum;
 
+
     static double func(double x) {
         return x * x * x;
     }
@@ -16,10 +17,10 @@ public class FuncMin {
                 minimum = func(x);
             } else {
                 minimum = min.a(minimum, func(x));
-                System.out.printf("x = %7f, func(x) = %7f, a(x) = %7f%n", x, func(x), min.a(minimum, func(x)));
 
                 System.out.printf("x = %7f, func(x) = %7f, a(x) = %7f%n", x, func(x), min.a(minimum, func(x)));
-        }
+
+            }
         }
         System.out.print("Minimal function value: " + minimum);
         System.out.println();
@@ -28,14 +29,24 @@ public class FuncMin {
     // В функции main() создаем объект безымянного класса:
     public static void main(String[] args) {
 
-
+        System.out.println("Noname class approach: ");
         printMin(-2, 2, 0.5, new MinValue() {
             @Override
-            public double a(double minimum, double current) {
-                return minimum < current ? minimum : current;
-
-
+            public double a(double x, double y) {
+                return x < y ? x : y;
             }
         });
+        System.out.println();
+
+        System.out.println("Lambda approach: ");
+        printMin(-2, 2, 0.5, (x, y) -> {
+            return x < y ? x : y;
+        });
+
+        FuncMin tmp = new FuncMin();
+
+
     }
+
+
 }
