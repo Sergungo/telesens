c05a22a660dbe7269bbf2d5b5023ae7bdb52724d
Sergey Golenko <serjgolenko@gmail.com>
2019-01-14 16:55:43 +0200

    code modified

--- src/ua/sgolenko/telesense/tasks6/FuncMin.java
+++ src/ua/sgolenko/telesense/tasks6/FuncMin.java
@@ -1,12 +1,37 @@
 package ua.sgolenko.telesense.tasks6;
 
-public class FuncMin {
+public class FuncMin implements MinValue {
 
     static double minimum;
 
+    double func2(double x) {
+        return x * x * x;
+    }
+
+    @Override
+    public double a(double x, double y) {
+        return x < y ? x : y;
+    }
 
     static double func(double x) {
         return x * x * x;
+    }
+
+    void printMin(double from, double to,
+                  double step) {
+        for (double x = from; x <= to; x += step) {
+            if (x == from) {
+                minimum = func(x);
+            } else {
+                minimum = a(minimum, func(x));
+
+                System.out.printf("x = %7f, func(x) = %7f, a(x) = %7f%n", x, func(x), a(minimum, func(x)));
+
+            }
+        }
+        System.out.print("Minimal function value: " + minimum);
+        System.out.println();
+
     }
 
     static void printMin(double from, double to,
@@ -43,9 +68,9 @@ public class FuncMin {
             return x < y ? x : y;
         });
 
+        System.out.println("Visible class approach: ");
         FuncMin tmp = new FuncMin();
-
-
+        tmp.printMin(-2,2,0.5);
     }
 
 
